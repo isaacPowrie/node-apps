57b23983633367ed74bf9eabfbb2d85f5e8c400d
Isaac Powrie <isaalawrencepowrie@gmail.com>
2019-09-22 19:46:53 -0400

    handlebars templating added

--- server.js
+++ server.js
@@ -1,6 +1,20 @@
 const express = require("express");
 const path = require("path");
+const exphbs = require("express-handlebars");
+
 const app = express();
+app.engine(".hbs", exphbs({ 
+    extname: ".hbs",
+    helpers: {
+        navLink: function(url, options){
+            return '<li' +
+                ((url == app.locals.activeRoute) ? ' class="nav-item active" ' : ' class="nav-item" ') + 
+                '><a class="nav-link" href="' + url + '">' + options.fn(this) + '</a></li>';
+        }
+    },
+    defaultLayout: "main"
+}));
+app.set("view engine", ".hbs");
 
 const HTTP_PORT = process.env.HTTP_PORT || 8080
 function onHttpStart() {
@@ -8,11 +22,21 @@ function onHttpStart() {
 }
 
 /*APPLICATION MIDDLEWARE*/
+// Active window middleware
+app.use(function(req,res,next){
+    let route = req.baseUrl + req.path;
+    app.locals.activeRoute = (route == "/") ? "/" : route.replace(/\/$/, "");
+    next();
+});
+
+// Static file middleware
 app.use(express.static("public"));
 
 /*APPLICATION ROUTING*/
 app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "/views/home.html"));
+    res.render("home", {
+        data: {}
+    });
 });
 
 app.use((req, res) => {
